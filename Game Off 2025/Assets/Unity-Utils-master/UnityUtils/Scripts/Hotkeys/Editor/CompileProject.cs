f33f7bde14ff024321ccee595611ae8717db37db
Niko Prants <enipr001@edu.xamk.fi>
2025-11-03 21:10:31 +0200

    back to og multiplayer

--- Game Off 2025/Assets/Unity-Utils-master/UnityUtils/Scripts/Hotkeys/Editor/CompileProject.cs
+++
@@ -1,9 +0,0 @@
-using UnityEditor;
-using UnityEditor.Compilation;
-
-public static class CompileProject {
-    [MenuItem("File/Compile _F5")]
-    static void Compile() {
-        CompilationPipeline.RequestScriptCompilation(RequestScriptCompilationOptions.CleanBuildCache);
-    }
-}
