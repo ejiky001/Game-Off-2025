22ba27bb25e9fa2b2682d85c97a5ba58537b5898
Niko Prants <enipr001@edu.xamk.fi>
2025-11-20 00:26:25 +0200

    maybe

--- Game Off 2025/Assets/Game/Code/Scripts/Objects/Button.cs
+++ Game Off 2025/Assets/Game/Code/Scripts/Objects/Button.cs
@@ -3,60 +3,28 @@ using Unity.Netcode;
 
 namespace Unity.Multiplayer.Center.NetcodeForGameObjects
 {
-    
     public class Button : NetworkBehaviour
     {
-        [Header("Button Settings")]
-        [Tooltip("Layer of objects that can press the button (e.g., 'Box').")]
-        [SerializeField] private LayerMask boxLayer;
 
-        
         public NetworkVariable<bool> IsPressed = new NetworkVariable<bool>(false);
 
-        private int currentBoxCount = 0;
-        internal bool pressed;
 
-        private bool IsBox(Collider other)
+        
+        public void PressButtonByProjectile()
         {
-            return ((1 << other.gameObject.layer) & boxLayer) != 0;
-        }
-
-       
-        private void OnTriggerEnter(Collider other)
-        {
-            if (!IsServer)
+            if (!IsServer)
             {
                 return;
             }
 
-            if (IsBox(other))
+            if (!IsPressed.Value)
             {
-                currentBoxCount++;
-
-                if (currentBoxCount > 0 && !IsPressed.Value)
-                {
-                    IsPressed.Value = true;
-                }
-            }
-        }
-
-       
-        private void OnTriggerExit(Collider other)
-        {
-            if (!IsServer)
-            {
-                return;
+                IsPressed.Value = true;
+                Debug.Log("[Button] Activated by projectile.");
             }
 
-            if (IsBox(other))
-            {
-                currentBoxCount = Mathf.Max(0, currentBoxCount - 1); 
+            
+        }
 
-                if (currentBoxCount == 0 && IsPressed.Value)
-                {
-                    IsPressed.Value = false;
-                }
-            }
-        }
-    }
+    }
 }
