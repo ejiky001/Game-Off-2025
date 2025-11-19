b2865e72e958802326642800073a0cd8f7e4acfe
Niko Prants <enipr001@edu.xamk.fi>
2025-11-19 20:36:48 +0200

    button update

--- Game Off 2025/Assets/Game/Code/Scripts/Objects/Button.cs
+++ Game Off 2025/Assets/Game/Code/Scripts/Objects/Button.cs
@@ -3,9 +3,60 @@ using Unity.Netcode;
 
 namespace Unity.Multiplayer.Center.NetcodeForGameObjects
 {
-
+    
     public class Button : NetworkBehaviour
     {
-        public bool pressed = false;
+        [Header("Button Settings")]
+        [Tooltip("Layer of objects that can press the button (e.g., 'Box').")]
+        [SerializeField] private LayerMask boxLayer;
+
+        
+        public NetworkVariable<bool> IsPressed = new NetworkVariable<bool>(false);
+
+        private int currentBoxCount = 0; 
+
+        
+        private bool IsBox(Collider other)
+        {
+            return ((1 << other.gameObject.layer) & boxLayer) != 0;
+        }
+
+       
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!IsServer)
+            {
+                return;
+            }
+
+            if (IsBox(other))
+            {
+                currentBoxCount++;
+
+                if (currentBoxCount > 0 && !IsPressed.Value)
+                {
+                    IsPressed.Value = true;
+                }
+            }
+        }
+
+       
+        private void OnTriggerExit(Collider other)
+        {
+            if (!IsServer)
+            {
+                return;
+            }
+
+            if (IsBox(other))
+            {
+                currentBoxCount = Mathf.Max(0, currentBoxCount - 1); 
+
+                if (currentBoxCount == 0 && IsPressed.Value)
+                {
+                    IsPressed.Value = false;
+                }
+            }
+        }
     }
 }
